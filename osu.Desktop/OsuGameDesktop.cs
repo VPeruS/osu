8449bac7b151badf0db2b529c9a56343c9ceb046
V.Kalyuzhny <vperus@gmail.com>
2017-10-19 03:55:02 +0300

    Switch to OpenTK drag&drop

--- osu.Desktop/OsuGameDesktop.cs
+++ osu.Desktop/OsuGameDesktop.cs
@@ -7,13 +7,12 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using Microsoft.Win32;
 using osu.Desktop.Overlays;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Platform;
 using osu.Game;
 using osu.Game.Screens.Menu;
+using OpenTK.Input;
 
 namespace osu.Desktop
 {
@@ -105,38 +104,22 @@ namespace osu.Desktop
                 desktopWindow.Icon = new Icon(Assembly.GetExecutingAssembly().GetManifestResourceStream(GetType(), "lazer.ico"));
                 desktopWindow.Title = Name;
 
-                desktopWindow.DragEnter += dragEnter;
-                desktopWindow.DragDrop += dragDrop;
+                desktopWindow.FileDrop += (sender, e) => { dragDrop(e); };
             }
         }
 
-        private void dragDrop(DragEventArgs e)
+        private void dragDrop(FileDropEventArgs e)
         {
-            // this method will only be executed if e.Effect in dragEnter gets set to something other that None.
-            var dropData = (object[])e.Data.GetData(DataFormats.FileDrop);
-            var filePaths = dropData.Select(f => f.ToString()).ToArray();
-
-            if (filePaths.All(f => Path.GetExtension(f) == @".osz"))
-                Task.Run(() => BeatmapManager.Import(filePaths));
-            else if (filePaths.All(f => Path.GetExtension(f) == @".osr"))
+            if (Path.GetExtension(e.FileName) == @".osz")
+                Task.Run(() => BeatmapManager.Import(e.FileName));
+            else if (Path.GetExtension(e.FileName) == @".osr")
                 Task.Run(() =>
                 {
-                    var score = ScoreStore.ReadReplayFile(filePaths.First());
+                    var score = ScoreStore.ReadReplayFile(e.FileName);
                     Schedule(() => LoadScore(score));
                 });
         }
 
         private static readonly string[] allowed_extensions = { @".osz", @".osr" };
-
-        private void dragEnter(DragEventArgs e)
-        {
-            // dragDrop will only be executed if e.Effect gets set to something other that None in this method.
-            bool isFile = e.Data.GetDataPresent(DataFormats.FileDrop);
-            if (isFile)
-            {
-                var paths = ((object[])e.Data.GetData(DataFormats.FileDrop)).Select(f => f.ToString()).ToArray();
-                e.Effect = allowed_extensions.Any(ext => paths.All(p => p.EndsWith(ext))) ? DragDropEffects.Copy : DragDropEffects.None;
-            }
-        }
     }
 }
